ec383e5866c96633a535be3d1a73c2742c8a81fa
G1rdo <156968637+G1rdo@users.noreply.github.com>
2024-01-22 11:21:44 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sudo apt-get update
+echo "Starting Pi Sensor interface setup"
+# Set the pins to be enabled
+raspi-config nonint do_i2c 0
+# Install python
+sudo apt-get install build-essential git python-dev python-smbus vscode
+
+#To update this file with running OS's options, use dpkg --get-selections and paste them
