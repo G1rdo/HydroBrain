ea657a05bfc67cde0fa2473c8a86a9a5efa89ed0
G1rdo <156968637+G1rdo@users.noreply.github.com>
2024-02-16 19:04:31 +0000

    Update setup.sh
    Fixed venv, added all pins

--- setup.sh
+++ setup.sh
@@ -2,18 +2,21 @@
 sudo apt update
 echo "Starting Pi Sensor interface setup"
 # Set the pins to be enabled
-raspi-config nonint do_i2c 0
-# Install python
-sudo apt-get install build-essential git python-dev python-smbus code nginx -y
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_serial_hw 0
+sudo raspi-config nonint do_serial_cons 0
+# Install dependencies
+sudo apt-get install build-essential git python3 code nginx mariadb-server -y
+
 
 #MariaDB
-sudo apt install libmariadb3 libmariadb-dev
 
 #Create venv for python packages 
 python3 -m venv hydrobrain_venv
 source hydrobrain_venv/bin/activate
+which python
 
-sudo pip3 install mariadb==1.1.9
+python3 -m pip install mariadb 
 
 #Website
 echo "WARNING: apache2 will be uninstalled if it is already on the system"
@@ -24,8 +27,8 @@ sudo apt-get autoremove
 #Moves the website folder from the hydrobrain folder into nginx
 sudo mv ~/HydroBrain/website /etc/nginx/sites-available
 
-sudo systemctl start nginx
+#sudo systemctl start nginx
 #Turns on website on device startup
-sudo systemctl enable nginx
+#sudo systemctl enable nginx
 
 #To update this file with running OS's options, use dpkg --get-selections and paste them
