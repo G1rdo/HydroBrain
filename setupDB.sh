a679eb1f05df73ad165b401eabce256ea2dc75f2
Robert Gordick <156968637+G1rdo@users.noreply.github.com>
2024-02-19 01:28:12 +0000

    Update setupDB.sh
    Added support for new databases

--- setupDB.sh
+++ setupDB.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
  
 #Get all the varaibles we need
-root_password=$(sed -nr "/^\[database\]/ { :l /^dataBaseMainPassword[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ./config.ini)
+root_password=$(sed -nr "/^\[database\]/ { :l /^dataBaseMainPassword[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
 echo $root_password
-supportedpHSensors=$(sed -nr "/^\[database\]/ { :l /^supportedpHSensors[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ./config.ini)
-echo $supportedpHSensors
-reader_password=$(sed -nr "/^\[database\]/ { :l /^dataBaseReaderPassword[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ./config.ini)
+reader_password=$(sed -nr "/^\[database\]/ { :l /^dataBaseReaderPassword[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
 echo $reader_password
+inserter_password=$(sed -nr "/^\[database\]/ { :l /^dataBaseInserterPassword[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $inserter_password
+
+supportedpHSensors=$(sed -nr "/^\[database\]/ { :l /^supportedpHSensors[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $supportedpHSensors
+pHUnits=$(sed -nr "/^\[database\]/ { :l /^PHUnits[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $pHUnits
+supportedDOSensors=$(sed -nr "/^\[database\]/ { :l /^supportedDOSensors[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $supportedDOSensors
+DOUnits=$(sed -nr "/^\[database\]/ { :l /^DOUnits[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $DOUnits
+supportedECSensors=$(sed -nr "/^\[database\]/ { :l /^supportedECSensors[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $supportedECSensors
+ECUnits=$(sed -nr "/^\[database\]/ { :l /^ECUnits[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $ECUnits
+supportedTimeSensors=$(sed -nr "/^\[database\]/ { :l /^supportedTimeSensors[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $supportedTimeSensors
+TimeUnits=$(sed -nr "/^\[database\]/ { :l /^TimeUnits[ ]*=/ { s/[^=]*=[ ]*//; p; q;}; n; b l;}" ~/HydroBrain/config.ini)
+echo $TimeUnits
+
 
 #This is a replacement for Mariadb-secure-installation
 sudo mariadb -e "SET PASSWORD FOR root@localhost = PASSWORD(\"$root_password\");FLUSH PRIVILEGES;" 
@@ -16,38 +34,59 @@ sudo mariadb -e "DROP DATABASE IF EXISTS test"
 sudo mariadb -e "FLUSH PRIVILEGES"
 
 
-#Create database and fill it with some data 
+#Create database and fill it with tables
 sudo mariadb -e "CREATE DATABASE IF NOT EXISTS sensor_data"
+
 sudo mariadb -e "use sensor_data;CREATE TABLE IF NOT EXISTS ph ( \
     id INT NOT NULL auto_increment PRIMARY KEY, \
     probe_name ENUM($supportedpHSensors), \
     ph decimal (8, 6), \
-    sql_timestamp TIMESTAMP, \
+    units ENUM($pHUnits), \
+    sql_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
     sensor_timestamp TIMESTAMP \
     ) ENGINE=INNODB;" \
  
 echo "pH table created."
 
-echo "Inserting data into pH table..."
+
+sudo mariadb -e "use sensor_data;CREATE TABLE IF NOT EXISTS dissolved_oxygen ( \
+    id INT NOT NULL auto_increment PRIMARY KEY, \
+    probe_name ENUM($supportedDOSensors), \
+    dissolved_oxygen decimal (13, 6), \
+    units ENUM($DOUnits), \
+    sql_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
+    sensor_timestamp TIMESTAMP \
+    ) ENGINE=INNODB;" \
  
+echo "DO table created."
+
+sudo mariadb -e "use sensor_data;CREATE TABLE IF NOT EXISTS electrical_conductivity ( \
+    id INT NOT NULL auto_increment PRIMARY KEY, \
+    probe_name ENUM($supportedECSensors), \
+    electrical_conductivity decimal (13, 6), \
+    units ENUM($ECUnits), \
+    sql_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
+    sensor_timestamp TIMESTAMP \
+    ) ENGINE=INNODB;" \
  
-query1="use sensor_data; INSERT INTO ph (probe_name, ph, sensor_timestamp) \
-        VALUES('PH-BTA', 3.14149, '2024-02-06 00:33:06+00:00')"
-query2="use sensor_data; INSERT INTO ph (probe_name, ph, sensor_timestamp) \
-        VALUES('PH-BTA', 3.14149, '2024-02-06 00:33:06+00:00')"
-query3="use sensor_data; INSERT INTO ph (probe_name, ph, sensor_timestamp) \
-        VALUES('PH-BTA', 30.222, '2024-02-06 00:33:06+00:00')"
-query4="use sensor_data; INSERT INTO ph (probe_name, ph, sensor_timestamp) \
-        VALUES('E-312', 11.0011111111, '2024-02-06 00:33:06+00:00')"
+echo "EC table created."
+
+sudo mariadb -e "use sensor_data;CREATE TABLE IF NOT EXISTS time ( \
+    id INT NOT NULL auto_increment PRIMARY KEY, \
+    probe_name ENUM($supportedTimeSensors), \
+    time decimal (13, 6), \
+    units ENUM($TimeUnits), \
+    sql_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
+    sensor_timestamp TIMESTAMP \
+    ) ENGINE=INNODB;" \
  
-sudo mariadb -e "$query1"
-sudo mariadb -e "$query2"
-sudo mariadb -e "$query3"
-sudo mariadb -e "$query4"
+echo "Time table created."
  
  
 #Grant privileges to the website server to read data from the sensor_data database. 
 sudo mariadb -e "CREATE USER IF NOT EXISTS 'site_reader'@'localhost' IDENTIFIED BY '$reader_password'"
 sudo mariadb -e "GRANT SELECT ON sensor_data.* TO 'site_reader'@'localhost' IDENTIFIED BY '$reader_password';"
+sudo mariadb -e "CREATE USER IF NOT EXISTS 'data_inserter'@'localhost' IDENTIFIED BY '$inserter_password'"
+sudo mariadb -e "GRANT INSERT ON sensor_data.* TO 'data_inserter'@'localhost' IDENTIFIED BY '$inserter_password';"
 # Make our changes take effect
 sudo mariadb -e "FLUSH PRIVILEGES"
