5ba79c723238e0c6bcabc7b39d172b39b2d606d3
Robert Gordick <156968637+G1rdo@users.noreply.github.com>
2024-03-05 01:04:11 +0000

    Update data.php
    Better config reading errors

--- nginx/website/data.php
+++ nginx/website/data.php
@@ -1,6 +1,17 @@
 <?php
 $HYDROBRAINHOME = "Stringtoreplace";
-$cfgArray = parse_ini_file("${HYDROBRAINHOME}/HydroBrain/config.ini");
+$cfgLocation = $HYDROBRAINHOME . "/HydroBrain/config.ini";
+try {
+    $cfgArray = parse_ini_file($cfgLocation);
+    if (!(is_readable($cfgLocation))) {
+        throw new Exception("Parsing of config file failed, no read permission.");
+    } elseif ($cfgArray == "") {
+        throw new Exception("Parsing of config file with parse_ini_file returned as empty. Likely cause is a syntax error in the config.");
+        #Make sure there are no banned characters in the file.
+    }
+} catch (Exception $e) {
+    print $e->getMessage();
+}
 $user = 'site_reader';
 $password = $cfgArray['dataBaseReaderPassword'];
 $database = "sensor_data";
