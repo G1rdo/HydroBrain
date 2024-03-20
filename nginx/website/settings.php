13cf8f5518d4c69e2ba4bec2ab906cd68c478349
Robert Gordick <156968637+G1rdo@users.noreply.github.com>
2024-03-20 13:17:35 -0400

    Update settings.php
    Added search engine description and keywords

--- nginx/website/settings.php
+++ nginx/website/settings.php
@@ -22,8 +22,8 @@ function loadPage() {
 <head>
     <?php require 'assets/html/standardHead.html';?>
 
-    <meta name="description" content="HydroBrain school home page">
-    <meta name="keywords" content="HydroBrain, home, school, index">
+    <meta name="description" content="HydroBrain config page">
+    <meta name="keywords" content="settings, config, configuration, HydroBrain, school">
     <!-- If you choose to modify your schools HydroBrain site, feel free to put your name below -->
     <meta name="author" content="">
     <title>HydroBrain</title>
