53cac5c64b56c0bbb50f878b3d49a341b167a36e
Robert Gordick <156968637+G1rdo@users.noreply.github.com>
2024-03-20 13:16:35 -0400

    Update settings.php
    Added http

--- nginx/website/settings.php
+++ nginx/website/settings.php
@@ -16,3 +16,21 @@ function loadPage() {
   echo("page loaded");
 }
 ?>
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <?php require 'assets/html/standardHead.html';?>
+
+    <meta name="description" content="HydroBrain school home page">
+    <meta name="keywords" content="HydroBrain, home, school, index">
+    <!-- If you choose to modify your schools HydroBrain site, feel free to put your name below -->
+    <meta name="author" content="">
+    <title>HydroBrain</title>
+</head>
+
+<body>
+    <?php require 'assets/html/header.html';?>
+</body>
+
+</html>
