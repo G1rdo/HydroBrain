f283fe23ae8e88f7d364d6206b172c3dd9f34fa5
Robert Gordick <156968637+G1rdo@users.noreply.github.com>
2025-02-23 19:57:44 -0500

    Create ads1005.py

---
+++ sensorPrograms/adcDrivers/ads1005.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Slightly modified verison of https://github.com/adafruit/Adafruit_CircuitPython_ADS1x15/blob/main/examples/ads1x15_simpletest.py
+
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1005 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1005(i2c)
+
+# Create single-ended input on channel 0
+chan0 = AnalogIn(ads, ADS.P0)
+
+while True:
+    print(chan0.voltage)
+    #print(1.3)
+    time.sleep(0.5)
